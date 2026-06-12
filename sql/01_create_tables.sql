0a03a0cabd433592bf0d638309b9de0fe727e8fd
Daniella Shalom <daniellashalom9@gmail.com>
2026-06-12 12:56:39 +0000

    First table created

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -1,4 +1,10 @@
 -- Customers
+DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (
+    customer_id VARCHAR(60) PRIMARY KEY,
+    customer_unique_id VARCHAR(60),
+    customer_zip_code_prefix CHAR(5),
+    customer_city VARCHAR(50),
+    customer_state VARCHAR(30)
+);
 
-)
