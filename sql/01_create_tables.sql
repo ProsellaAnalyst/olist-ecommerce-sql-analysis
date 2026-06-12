ddb705d9d17ae03082b3d9cf1da78496955f66ed
Daniella Shalom <daniellashalom9@gmail.com>
2026-06-12 18:06:09 +0000

    Created orders

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -3,32 +3,58 @@ DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (
     customer_id VARCHAR(60) PRIMARY KEY,
     customer_unique_id VARCHAR(60),
-    customer_zip_code_prefix CHAR(5),
+    customer_zip_code_prefix VARCHAR(10),
     customer_city VARCHAR(50),
     customer_state VARCHAR(30)
-); 
+);
 
 -- Order Items
 DROP TABLE IF EXISTS order_items;
 CREATE TABLE order_items (
     order_id VARCHAR(50),
-    order_item_id VARCHAR(30),
+    order_item_id INT,
     product_id VARCHAR(50),
     seller_id VARCHAR(50),
     shipping_limit_date TIMESTAMP,
     price NUMERIC(10,2),
-    freight_value NUMERIC(10,2)
+    freight_value NUMERIC(10,2),
+    PRIMARY KEY (order_id, order_item_id)
 );
-
 
 -- Order Payments
 DROP TABLE IF EXISTS order_payments;
-CREATE TABLE order_payments(
-    order_id VARCHAR(50),
-    payments_sequential CHAR(30),
+CREATE TABLE order_payments (
+    order_id CHAR(32),
+    payment_sequential INT,
     payment_type VARCHAR(20),
-    payments_installments CHAR(10),
+    payment_installments INT,
     payment_value NUMERIC(10,2)
-)
+);
 
-SELECT * FROM order_payments
+-- Order Reviews
+DROP TABLE IF EXISTS order_reviews;
+CREATE TABLE order_reviews (
+    review_id CHAR(32) PRIMARY KEY,
+    order_id CHAR(32),
+    review_score INT CHECK (review_score BETWEEN 1 AND 5),
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date TIMESTAMP,
+    review_answer_timestamp TIMESTAMP
+);
+
+-- Orders
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+    order_id VARCHAR(60) PRIMARY KEY,
+    customer_id VARCHAR(60),
+    order_status VARCHAR(20),
+    order_purchase_timestamp TIMESTAMP,
+    order_approved_at TIMESTAMP,
+    order_delivered_carrier_date TIMESTAMP,
+    order_delivered_customer_date TIMESTAMP,
+    order_estimated_delivery_date TIMESTAMP,
+
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
