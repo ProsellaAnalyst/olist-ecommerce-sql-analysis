14eef1ab3998b023a7427aeedb2e6b42e27e772f
Daniella Shalom <daniellashalom9@gmail.com>
2026-06-12 14:16:40 +0000

    adding codes

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -6,5 +6,19 @@ CREATE TABLE customers (
     customer_zip_code_prefix CHAR(5),
     customer_city VARCHAR(50),
     customer_state VARCHAR(30)
+); 
+
+
+-- Order Items
+DROP TABLE IF EXISTS order_items;
+CREATE TABLE order_items (
+    order_id VARCHAR(50),
+    order_item_id VARCHAR(30),
+    product_id VARCHAR(50),
+    seller_id VARCHAR(50),
+    shipping_limit_date TIMESTAMP,
+    price NUMERIC(10,2),
+    freight_value NUMERIC(10,2)
 );
+
 
