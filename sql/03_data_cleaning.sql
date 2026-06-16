d08acc2b0124e1e0d85a1d23c5a334f18768b186
Daniella Shalom <daniellashalom9@gmail.com>
2026-06-16 12:31:16 +0000

    Editted data cleaning, added new file

--- sql/03_data_cleaning.sql
+++ sql/03_data_cleaning.sql
@@ -1,18 +1,15 @@
-SELECT * FROM customers;
-
-SELECT * FROM  product_category_translation;
-
-SELECT * FROM sellers;
-
-SELECT * FROM  products;
-
-SELECT * FROM orders;
-
-SELECT * FROM order_items;
-
-SELECT * FROM order_payments;
-
-SELECT * FROM order_reviews;
-
-
-
+SELECT 'customers' AS table_name, COUNT(*) AS row_count FROM customers
+UNION ALL
+SELECT 'orders', COUNT(*) FROM orders
+UNION ALL
+SELECT 'order_items', COUNT(*) FROM order_items
+UNION ALL
+SELECT 'order_payments', COUNT(*) FROM order_payments
+UNION ALL
+SELECT 'order_reviews', COUNT(*) FROM order_reviews
+UNION ALL
+SELECT 'products', COUNT(*) FROM products
+UNION ALL
+SELECT 'sellers', COUNT(*) FROM sellers
+UNION ALL
+SELECT 'product_category_translation', COUNT(*) FROM product_category_translation;
