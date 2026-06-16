ebc6af7b7d357a4387f0ee37772b79f28bda29a8
Daniella Shalom <daniellashalom9@gmail.com>
2026-06-16 02:55:47 +0000

    Started data cleaning

---
+++ sql/03_data_cleaning.sql
@@ -0,0 +1,18 @@
+SELECT * FROM customers;
+
+SELECT * FROM  product_category_translation;
+
+SELECT * FROM sellers;
+
+SELECT * FROM  products;
+
+SELECT * FROM orders;
+
+SELECT * FROM order_items;
+
+SELECT * FROM order_payments;
+
+SELECT * FROM order_reviews;
+
+
+
